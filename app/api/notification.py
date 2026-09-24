4ad8b584fec1f8152a11e36f8c9cd0a2a42259de
leoneljfernandes <leojfernandes@protonmail.com>
2026-09-24 07:07:26 -0300

    Implementacion de /api/notification.py

--- app/api/notification.py
+++ app/api/notification.py
@@ -1,17 +1,15 @@
-from datetime import timedelta
-
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.auth import create_access_token, verify_password
-from app.crud.notification import get_notifications_by_user_id
+from app.crud.notification import get_notification_by_id, get_notifications_by_user_id
 from app.models.user import User
 from app.schemas.notification import NotificationResponse
-from app.schemas.user import UserCreate, UserResponse, UserUpdate, Token
-from app.crud.user import create_user, get_users, get_user, update_user, delete_user, get_user_by_email
+from app.schemas.user import UserResponse
 from app.database import get_db
 from app.auth import get_current_user, get_current_admin
 from app.config import settings
+from app.schemas.notification import NotificationCreate, NotificationUpdate
+from app.services.dispatcher import NotificationDispatcher
+from app.crud.notification import create_notification, update_notification, delete_notification
 
 router = APIRouter()
 
@@ -25,3 +23,55 @@ async def get_my_notifications(
     return notifications
 
 
+@router.post('/create-notification', response_model=NotificationResponse, status_code=status.HTTP_201_CREATED)
+async def post_new_notification(
+    notification: NotificationCreate,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    db_notif = await create_notification(db, notification, current_user.id)
+
+    await NotificationDispatcher.dispatch(db_notif)
+    return db_notif
+
+
+@router.put('/update-notification/{notification_id}', response_model=NotificationResponse)
+async def update_a_notification(
+    notification_id: int,
+    notification: NotificationUpdate,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    existing_notification = await get_notification_by_id(db, notification_id)
+    if not existing_notification:
+        raise HTTPException(status_code=404, detail='Notification not found')
+
+    if existing_notification.user_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not authorized to update this notification",
+        )
+
+    updated_notification = await update_notification(db, notification_id, notification)
+    if updated_notification is None:
+        raise HTTPException(status_code=404, detail='Notification not found')
+    return updated_notification    
+
+@router.delete('/delete-notification/{notification_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_a_notification(
+    notification_id: int,
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    notification = await get_notification_by_id(db, notification_id)
+    if not notification:
+        raise HTTPException(status_code=404, detail='Notification not found')
+
+
+    if notification.user_id != current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not authorized to delete this notification",
+        )
+
+    await delete_notification(db, notification_id)
